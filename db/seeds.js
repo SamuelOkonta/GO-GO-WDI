b5a60961f2eefc7e5665537f2eb9c3746359fda0
Samuel Okonta <inryo@Samuels-MacBook-Air.local>
2019-03-26 10:37:53 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/seeds.js
+++ db/seeds.js
@@ -3,9 +3,18 @@ const Genre = require('../models/Genre.js')
   
 // using Promises
 Anime.deleteMany().then(() => {
-    const luke = new Anime({name: 'Inuyasha', })
-    return luke.save()
+    const Inuyasha = new Anime({name: 'Inuyasha', })
+    return Inuyasha.save()
   }).then(() => {
     const darth = new Anime({name: 'Darth Vader', })
     return darth.save()
   })
+
+  Genre.deleteMany().then(() => {
+    const Inuyasha = new Genre({name: 'Inuyasha', })
+    return Inuyasha.save()
+  }).then(() => {
+    const darth = new Genre({name: 'Darth Vader', })
+    return darth.save()
+  })
+  
