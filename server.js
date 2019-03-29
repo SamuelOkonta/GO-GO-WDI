6377f9f0371719fbf6277191471a4af63265d2f0
Samuel Okonta <inryo@Samuels-MacBook-Air.local>
2019-03-29 12:28:52 -0400

    restructured server

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const bodyParser = require ('body-parser')
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(__dirname + '/../client/build/index.html'))
 app.use(bodyParser.json())
 
 app.use('/api/v1', routes)
 
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
+    res.sendFile(__dirname + '/../client/build/index.html')
 })
 
 const PORT = process.env.PORT || 3001
